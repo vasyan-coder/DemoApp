35401797e8865a2f4508953806d3ab3f69100e99
vasyan <vasyan.coder@gmail.com>
2023-09-06 23:07:47 +0300

    fix: removed moshi from domain

--- app/src/main/java/com/vasyancoder/demoapp/domain/entity/DataGif.kt
+++ app/src/main/java/com/vasyancoder/demoapp/domain/entity/DataGif.kt
@@ -1,15 +1,9 @@
 package com.vasyancoder.demoapp.domain.entity
 
 
-import com.squareup.moshi.Json
-
 data class DataGif(
-    @Json(name = "id")
     val id: String,
-    @Json(name = "title")
     val title: String,
-    @Json(name = "url")
     val url: String,
-    @Json(name = "username")
     val username: String
 )
--- app/src/main/java/com/vasyancoder/demoapp/domain/entity/Gif.kt
+++ app/src/main/java/com/vasyancoder/demoapp/domain/entity/Gif.kt
@@ -1,9 +1,6 @@
 package com.vasyancoder.demoapp.domain.entity
 
 
-import com.squareup.moshi.Json
-
 data class Gif(
-    @Json(name = "data")
     val dataGif: List<DataGif>
 )
